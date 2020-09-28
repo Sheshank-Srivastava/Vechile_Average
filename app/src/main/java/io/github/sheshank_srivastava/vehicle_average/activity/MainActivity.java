9334f8e704cf6ef90e47f43eedd0fc30bbdf7b38
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2020-09-28 17:39:01 +0530

    first commit

---
+++ app/src/main/java/io/github/sheshank_srivastava/vehicle_average/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package io.github.sheshank_srivastava.vehicle_average.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.sheshank_srivastava.vehicle_average.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
